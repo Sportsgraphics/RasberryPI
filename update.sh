42d04320e03d8eec3075d36e447b1dd57297e040
Sportsgraphics <Sportsgraphics@users.noreply.github.com>
2019-12-15 15:55:12 +1100

    Update update.sh

--- update.sh
+++ update.sh
@@ -5,3 +5,7 @@ sudo apt-get -y install git
 sudo apt-get install nodejs
 
 sudo apt-get -y i2c-tools
+sudo apt-get install libavahi-compat-libdnssd-dev
+
+cd ~
+git clone https://github.com/johnwargo/Seeed-Studio-Relay-Board
