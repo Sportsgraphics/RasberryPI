64e4c08db8e983e0b5411747d56f117628802629
Sportsgraphics <Sportsgraphics@users.noreply.github.com>
2019-12-15 15:30:13 +1100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,7 @@
-sudo apt-get upgrade
-sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y update
+
+sudo apt-get -y install git
+sudo apt-get -y install node-js
+
+sudo apt-get -y i2c-tools
